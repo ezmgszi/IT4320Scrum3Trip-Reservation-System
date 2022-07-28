0d827bd138ddc3b4417356076876f24ed0c9c921
ezmgszi <zaclipperd@gmail.com>
2022-07-27 23:13:23 -0500

    added functionality to admin page

--- it-4320-final-proj/it-4320-final-proj/flask_wtforms_tutorial/routes.py
+++ it-4320-final-proj/it-4320-final-proj/flask_wtforms_tutorial/routes.py
@@ -7,6 +7,40 @@ import itertools
 import csv
 
 
+# check if username and pass word are correct
+def validate_user_pass(user_name, password):
+    print(user_name)
+    print(password)
+    data = pd.read_csv('passcodes.txt', header=None, names=['user_name', 'password'])
+    print(data)
+    data = data[data.user_name == user_name]
+    print(data)
+    print(password)
+    print(data.password)
+    data = data[data.password.astype(str) == password]
+    print(data)
+    if data.empty:
+        return True
+    else:
+        return False
+
+
+# get total sales of reserved seats
+def get_total_sales(reservation_list):
+    seating_chart= {}
+    for i in range(0, 12):
+        seating_chart[i] = [0, 0, 0, 0]
+    for line in reservation_list:
+        seating_chart[int(line[1])][int(line[2])] = 1
+    total_sales = 0
+    seating_chart_prices = [100, 75, 50, 100]
+    for i in range(0, 12):
+        for j in range(0, 3):
+            if seating_chart[i][j] == 1:
+                total_sales = total_sales + seating_chart_prices[j]
+    return total_sales
+
+
 # returns true if there is no other reservation for that seat
 def validate_choice(row, seat):
     data = pd.read_csv('reservations.csv', header=None, usecols=[1, 2], names=['row', 'seat'])
@@ -60,31 +94,48 @@ def generate_seating_charts(reservation_list):
     return seating_chart
 
 
-#@app.route("/", methods=['GET', 'POST'])
 @app.route("/", methods=['GET', 'POST'])
 def user_options():
 
     form = UserOptionForm()
-    #check if the request method is POST. POST method means that form data was submitted
-    #So, if method is POST we can get the form data
+    # check if the request method is POST. POST method means that form data was submitted
+    # So, if method is POST we can get the form data
     if request.method == 'POST' and form.validate_on_submit():
         option = request.form['option']
 
         if option == "1":
-            #if form option is "1" go to the admin page
+            # if form option is "1" go to the admin page
             return redirect('/admin')
         else:
-            #if form option is "2" go to the reservations page
+            # if form option is "2" go to the reservations page
             return redirect("/reservations")
 
     return render_template("options.html", form=form, template="form-template")
 
+
 @app.route("/admin", methods=['GET', 'POST'])
 def admin():
-
+    # posting data used for checking if statment in templete, allows for display for first page load, and submitted
+    posting_data = False
+    # get the form data
     form = AdminLoginForm()
+    # if submit was selected, then a requests will have a post method
+    if request.method == "POST" and form.validate_on_submit():
+        # validate pass word
+        validate_input = validate_user_pass(request.form["username"], request.form["password"])
+        print(validate_input)
+        if not validate_input:
+            # create seating chart list
+            initial_reservations_list = get_reservations_list('reservations.csv')
+            seating_chart = generate_seating_charts(initial_reservations_list)
+            # get total price of sales
+            total_sales = get_total_sales(initial_reservations_list)
+            return render_template("admin.html", form=form, template="form-template", seating_chart=seating_chart,
+                                   validate_input=validate_input, total_sales=total_sales)
+        return render_template("admin.html", form=form, template="form-template", posting_data=posting_data,
+                               validate_input=validate_input)
+    return render_template("admin.html", form=form, template="form-template", posting_data=posting_data)
 
-    return render_template("admin.html", form=form, template="form-template")
 
 @app.route("/reservations", methods=['GET', 'POST'])
 def reservations():
@@ -101,7 +152,7 @@ def reservations():
         last_name = request.form["last_name"]
         row_choice = int(request.form["row"]) - 1
         seat_choice = int(request.form["seat"]) - 1
-        # make sure reservation isnt already done
+        # make sure reservation isn't already done
         validated_choice = validate_choice(row_choice, seat_choice)
         # seat choice is available to be reserved, generate code and add to list
         if validated_choice:
